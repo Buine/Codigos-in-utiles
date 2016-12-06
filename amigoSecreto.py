190dea1a9b63fc5f64b52dde173972d42701f80c
Javier Palma-Espinosa <javier.palmae@gmail.com>
2016-12-06 19:26:50 -0300

    Update amigoSecreto.py
    modified sendmail procedure

--- amigoSecreto.py
+++ amigoSecreto.py
@@ -8,13 +8,12 @@ import random
 import smtplib
 
 
-def sendemail(from_addr, to_addr_list, cc_addr_list,
+def sendemail(from_addr, to_addr_list,
               subject, message,
               login, password,
               smtpserver='smtp.gmail.com:587'):
     header  = 'From: %s\n' % from_addr
     header += 'To: %s\n' % ','.join(to_addr_list)
-    header += 'Cc: %s\n' % ','.join(cc_addr_list)
     header += 'Subject: %s\n\n' % subject
     message = header + message
  
